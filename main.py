e2bc7c33efed134580a3790aefde9e3ddaacddb5
Erickson <erickdak@iu.edu>
2019-08-28 17:08:50 -0400

    i did it

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('My name is Dakota Erickson')
+print('My favorite game as of right now is Slay the Spire')
+print('My concern with this class is that I have not used Godot before')
+print('In general, I am excited to go to Subway after class because I did not eat before class and am very hungry. Otherwise, I am excited to work with new students in GDIU')
+print('My stack overflow number is 11991292')
+print('https://github.com/Dakota-Erickson')
